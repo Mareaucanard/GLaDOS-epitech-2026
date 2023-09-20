4a56c18124898bf6c382474e929508d25350eda2
Matthieu Fraiz <matthieu.fraiz@epitech.eu>
2023-09-20 10:15:57 +0200

    Add example for lambda

--- src/DefaultSymbol.hs
+++ src/DefaultSymbol.hs
@@ -1,41 +1,41 @@
 module DefaultSymbol (defaultSymbols) where
 
+import Ast (Ast (..), VarMap)
 import qualified Data.Map.Lazy as Map
-import Ast (Ast (..), VarMap (..))
 
-addAst :: [Ast] -> Either Ast String
-addAst [Value a, Value b] = Left (Value (a + b))
-addAst _ = Right "Invalid use of add operation"
+addAst :: [Ast] -> VarMap -> Either Ast String
+addAst [Value a, Value b] _ = Left (Value (a + b))
+addAst _ _ = Right "Invalid use of add operation"
 
-subAst :: [Ast] -> Either Ast String
-subAst [Value a, Value b] = Left (Value (a - b))
-subAst _ = Right "Invalid use of sub operation"
+subAst :: [Ast] -> VarMap -> Either Ast String
+subAst [Value a, Value b] _ = Left (Value (a - b))
+subAst _ _ = Right "Invalid use of sub operation"
 
-mulAst :: [Ast] -> Either Ast String
-mulAst [Value a, Value b] = Left (Value (a * b))
-mulAst _ = Right "Invalid use of times operation"
+mulAst :: [Ast] -> VarMap -> Either Ast String
+mulAst [Value a, Value b] _ = Left (Value (a * b))
+mulAst _ _ = Right "Invalid use of times operation"
 
-divAst :: [Ast] -> Either Ast String
-divAst [Value _, Value 0] = Right "Division by zero"
-divAst [Value a, Value b] = Left (Value (a `quot` b))
-divAst _ = Right "Invalid use of div operation"
+divAst :: [Ast] -> VarMap -> Either Ast String
+divAst [Value _, Value 0] _ = Right "Division by zero"
+divAst [Value a, Value b] _ = Left (Value (a `quot` b))
+divAst _ _ = Right "Invalid use of div operation"
 
-modAst :: [Ast] -> Either Ast String
-modAst [Value _, Value 0] = Right "Module by zero"
-modAst [Value a, Value b] = Left (Value (a `mod` b))
-modAst _ = Right "Invalid use of mod operation"
+modAst :: [Ast] -> VarMap -> Either Ast String
+modAst [Value _, Value 0] _ = Right "Module by zero"
+modAst [Value a, Value b] _ = Left (Value (a `mod` b))
+modAst _ _ = Right "Invalid use of mod operation"
 
-powAst :: [Ast] -> Either Ast String
-powAst [Value a, Value b] = Left (Value (a ^ b))
-powAst _ = Right "Invalid use of power operation"
+powAst :: [Ast] -> VarMap -> Either Ast String
+powAst [Value a, Value b] _ = Left (Value (a ^ b))
+powAst _ _ = Right "Invalid use of power operation"
 
 defaultSymbols :: VarMap
 defaultSymbols =
   Map.fromList
-    [ ("+", Lambda addAst),
-      ("-", Lambda subAst),
-      ("*", Lambda mulAst),
-      ("/", Lambda divAst),
-      ("%", Lambda modAst),
-      ("^", Lambda powAst)
+    [ ("+", Lambda "+" addAst),
+      ("-", Lambda "-" subAst),
+      ("*", Lambda "*" mulAst),
+      ("/", Lambda "/" divAst),
+      ("%", Lambda "%" modAst),
+      ("^", Lambda "^" powAst)
     ]
