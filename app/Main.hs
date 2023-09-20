54259622f2002f237f20495083096968a7a042f0
root <thibault.mir@epitech.eu>
2023-09-20 15:34:01 +0200

    Move interpreter functions in his own file

--- app/Main.hs
+++ app/Main.hs
@@ -3,23 +3,7 @@ module Main (main) where
 import Ast
 import Data.Char (toLower)
 import Lib (parseString)
-
-countParenthesis :: String -> Int -> Int
-countParenthesis [] n = n
-countParenthesis (x : xs) n
-  | x == '(' = countParenthesis xs (n + 1)
-  | x == ')' = countParenthesis xs (n - 1)
-  | otherwise = countParenthesis xs n
-
-concatStringIOString :: String -> IO String -> IO String
-concatStringIOString s1 io = io >>= (\s2 -> return (s1 ++ " " ++ s2))
-
-parseLineLogic :: Int -> String -> IO String
-parseLineLogic n line = if nbPar == 0 then return line else concatStringIOString line (parseLine nbPar)
-  where nbPar = countParenthesis line n
-
-parseLine :: Int -> IO String
-parseLine n = getLine >>= (\line -> parseLineLogic n line)
+import Interpreter (parseLine)
 
 processInput :: IO String
 processInput = do
