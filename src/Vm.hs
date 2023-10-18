904572801d7f6bb652458afd5d0af9ac4b0d240b
Ange Mouquet <ange.mouquet@epitech.eu>
2023-10-18 11:02:48 +0200

    Add basic operations

--- src/Vm.hs
+++ src/Vm.hs
@@ -13,10 +13,10 @@ data Value = Nb Int
     | Boolean Bool
     deriving Show
 
-data Op = Addition Int Int
-    | Substraction Int Int
-    | Multiplication Int Int
-    | Division Int Int
+data Op = Addition
+    | Substraction
+    | Multiplication
+    | Division
     deriving Show
 
 data Instructions = Push Value
@@ -27,21 +27,38 @@ type Stack = [Value]
 type Insts = [Instructions]
 
 exec :: Insts -> Stack -> (Value, Stack)
-exec ((Push val)::l) s = exec l (push val)
-exec ((Call Addition)::l) s = exec l (opStack s (+))
-exec ((Call Substraction)::l) s = exec l (opStack s (-))
-exec ((Call Multiplication)::l) s = exec l (opStack s (*))
-exec ((Call Division)::l) s = exec l (opStack s (/))
-exec (Ret r::l) s = pop s
+exec ((Push val):l) s = exec l (push s val)
+exec ((Call Addition):l) s = exec l (opStack s opAdd)
+exec ((Call Substraction):l) s = exec l (opStack s opSub)
+exec ((Call Multiplication):l) s = exec l (opStack s opMul)
+exec ((Call Division):l) s = exec l (opStack s opDiv)
+exec (Ret:l) s = pop s
+exec [] s = pop s
 
 push :: Stack -> Value -> Stack
 push s val = val : s
 
-pop :: Stack -> (Stack, Value)
-pop [] = ([], 0)
-pop (x:xs) = (xs, x)
+pop :: Stack -> (Value, Stack)
+pop [] = (Nb 0, [])
+pop (x:xs) = (x, xs)
 
-opStack :: Stack -> (Int -> Int -> Int) -> Stack
+opStack :: Stack -> (Value -> Value -> Value) -> Stack
 opStack s op = case pop s of
-    (tmp_stack, v1) -> case pop tmp_stack of
-        (final_stack, v2) -> push final_stack (op v1 v2)
+    (v1, tmp_stack) -> case pop tmp_stack of
+        (v2, final_stack) -> push final_stack (op v1 v2)
+
+opAdd :: Value -> Value -> Value
+opAdd (Nb a) (Nb b) = Nb (a + b)
+opAdd (Boolean a) (Boolean b) = Boolean (a || b)
+
+opSub :: Value -> Value -> Value
+opSub (Nb a) (Nb b) = Nb (a - b)
+opSub (Boolean a) (Boolean b) = Boolean (a && not b)
+
+opMul :: Value -> Value -> Value
+opMul (Nb a) (Nb b) = Nb (a * b)
+opMul (Boolean a) (Boolean b) = Boolean (a && b)
+
+opDiv :: Value -> Value -> Value
+opMul (Nb a) (Nb b) = Nb (a * b)
+opMul (Boolean a) (Boolean b) = Boolean (a && b)
