c8fae43c2e11825737f7e05cca84dde9973894b4
Matthieu Fraiz <matthieu.fraiz@epitech.eu>
2023-10-23 13:47:54 +0200

    Add ifblock ast to instructions

--- src/Instructions/AstToInstructions.hs
+++ src/Instructions/AstToInstructions.hs
@@ -1,8 +1,11 @@
 module Instructions.AstToInstructions (astListToInstructions) where
 
 import           Types (Ast(..), UnaryOperator(..), BinaryOperator(..)
-                      , Instruction(..), TernaryOperator(TernaryGate), IfType)
+                      , Instruction(..), TernaryOperator(TernaryGate), IfType
+                      , Value(Boolean))
 import qualified Types as T (Instruction(..))
+import           Basement.Compat.Base (Int64)
+import qualified Data.Bifunctor
 
 unaryToInst :: UnaryOperator -> Instruction
 unaryToInst BoolNot = NOT
@@ -27,6 +30,9 @@ binaryToInst BoolOr = OR
 ternaryToInst :: TernaryOperator -> Instruction
 ternaryToInst TernaryGate = TERNARY
 
+fi :: Int -> Int64
+fi = fromIntegral
+
 whileToInst :: Ast -> [Ast] -> [Instruction]
 whileToInst condition block =
   instCond ++ [JIF toEnd] ++ instBlock ++ [Jump toStart]
@@ -35,16 +41,40 @@ whileToInst condition block =
 
     instBlock = astListToInstructions block
 
-    toEnd = fromIntegral (length instBlock + 1)
+    toEnd = fi (length instBlock + 1)
 
-    toStart = fromIntegral $ -(length instBlock + length instCond + 1 + 1)
+    toStart = fi $ -(length instBlock + length instCond + 1 + 1)
 
 listToInst :: [Ast] -> [Instruction]
-listToInst elems = concatMap astToInst elems
-  ++ [LIST (fromIntegral (length elems))]
+listToInst elems = concatMap astToInst elems ++ [LIST (fi (length elems))]
 
-ifToInst :: [IfType] -> Maybe [Ast] -> [Instruction]
-ifToInst = undefined
+ifToInst :: [Instruction] -> [Instruction] -> Int64 -> Int64 -> [Instruction]
+ifToInst cond block endPos curPos =
+  cond ++ JIF toEndOfBlock:block ++ [Jump toEnd]
+  where
+    toEndOfBlock = fi (length block) + 1
+
+    toEnd = endPos - (curPos + fi (length cond) + 2 + fi (length block))
+
+ifsToInstLogic
+  :: [([Instruction], [Instruction])] -> Int64 -> Int64 -> [Instruction]
+ifsToInstLogic [] _ _ = []
+ifsToInstLogic ((cond, block):xs) endPos curPos =
+  ifToInst cond block endPos curPos
+  ++ ifsToInstLogic xs endPos (curPos + sizeOfItem)
+  where
+    sizeOfItem = fi (length cond) + 2 + fi (length block)
+
+ifsToInst :: [IfType] -> Maybe [Ast] -> [Instruction]
+ifsToInst ifs Nothing = ifsToInstLogic pairs sizeOfInsts 0
+  where
+    pairs = map (Data.Bifunctor.bimap astToInst astListToInstructions) ifs
+
+    sizeOfInsts = fi
+      $ length pairs * 2
+      + sum (map (\x -> length (fst x) + length (snd x)) pairs)
+ifsToInst ifs (Just block) =
+  ifsToInst (ifs ++ [(Const (Boolean True), block)]) Nothing
 
 astToInst :: Ast -> [Instruction]
 astToInst (Symbol s) = [PushSymbol s]
@@ -60,9 +90,10 @@ astToInst (FunctionCall name args) =
   concatMap astToInst (reverse args) ++ [PushSymbol name] ++ [Call]
 astToInst (FunctionDefinition name args block) =
   Function name args:astListToInstructions block
-astToInst (IfBlock x y z) = ifToInst (x:y) z
+astToInst (IfBlock x y z) = ifsToInst (x:y) z
 astToInst (WhileBlock cond block) = whileToInst cond block
-astToInst (ForBlock ini cond inc block) = astToInst ini ++ whileToInst cond (block ++ [inc])
+astToInst (ForBlock ini cond inc block) = astToInst ini
+  ++ whileToInst cond (block ++ [inc])
 astToInst (Return ast) = astToInst ast ++ [RET]
 astToInst (IndexOf symbol index) = astToInst index ++ [INDEX symbol]
 astToInst None = []
