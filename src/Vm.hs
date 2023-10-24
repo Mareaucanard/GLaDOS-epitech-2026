fe41c5b7de8c2a88a4c5110204817dd56b9c7cdb
Ange Mouquet <ange.mouquet@epitech.eu>
2023-10-24 10:51:29 +0200

    Adjust to IO format

--- src/Vm.hs
+++ src/Vm.hs
@@ -13,6 +13,7 @@ import Data.Int (Int64)
 
 data Value = Nb Int
     | Boolean Bool
+    | Str String
     deriving (Show, Eq)
 
 data Symbol = Val Value
@@ -57,14 +58,26 @@ data Instruction = Function String [String]
 type Stack = [Value]
 type Insts = [Instruction]
 
-exec :: Insts -> Stack -> [(String, Symbol)] -> Insts -> (Value, Stack)
+exec :: Insts -> Stack -> [(String, Symbol)] -> Insts -> IO (Value, Stack)
 exec ((Push val):l) s vTab past = exec l (push s val) vTab (Push val:past)
-exec (ADD:l) s vTab past = exec l (opStack s opAdd) vTab (ADD:past)
-exec (SUB:l) s vTab past = exec l (opStack s opSub) vTab (SUB:past)
-exec (MUL:l) s vTab past = exec l (opStack s opMul) vTab (MUL:past)
-exec (DIV:l) s vTab past = exec l (opStack s opDiv) vTab (DIV:past)
-exec (Vm.EQ:l) s vTab past = exec l (opStack s opEq) vTab (Vm.EQ:past)
-exec (Vm.LT:l) s vTab past = exec l (opStack s opLess) vTab (Vm.LT:past)
+exec (ADD:l) s vTab past = do
+    s' <- opStack s opAdd
+    exec l s' vTab (ADD:past)
+exec (SUB:l) s vTab past = do
+    s' <- opStack s opSub
+    exec l s' vTab (SUB:past)
+exec (MUL:l) s vTab past = do
+    s' <- opStack s opMul
+    exec l s' vTab (MUL:past)
+exec (DIV:l) s vTab past = do
+    s' <- opStack s opDiv
+    exec l s' vTab (DIV:past)
+exec (Vm.EQ:l) s vTab past = do
+    s' <- opStack s opEq
+    exec l s' vTab (Vm.EQ:past)
+exec (Vm.LT:l) s vTab past = do
+    s' <- opStack s opLess
+    exec l s' vTab (Vm.LT:past)
 exec ((JIF jmp):l) s vTab past = jumpIfFalse (JIF jmp:l) (fromIntegral jmp) s vTab past
 exec (RET:l) s _ _ = pop s
 exec [] s _ _ = pop s
@@ -72,20 +85,21 @@ exec [] s _ _ = pop s
 push :: Stack -> Value -> Stack
 push s val = val:s
 
-pop :: Stack -> (Value, Stack)
-pop [] = (Nb 0, [])
-pop (x:xs) = (x, xs)
+pop :: Stack -> IO (Value, Stack)
+pop [] = return (Nb 0, [])
+pop (x:xs) = return (x, xs)
 
-opStack :: Stack -> (Value -> Value -> Value) -> Stack
-opStack s op = case pop s of
-    (v1, tmp_stack) -> case pop tmp_stack of
-        (v2, final_stack) -> push final_stack (op v1 v2)
+opStack :: Stack -> (Value -> Value -> Value) -> IO Stack
+opStack s op = do
+    (v1, tmp_stack) <- pop s
+    (v2, final_stack) <- pop tmp_stack
+    return (push final_stack (op v1 v2))
 
-jumpIfFalse :: Insts -> Int -> Stack -> [(String, Symbol)] -> Insts -> (Value, Stack)
+jumpIfFalse :: Insts -> Int -> Stack -> [(String, Symbol)] -> Insts -> IO (Value, Stack)
 jumpIfFalse [] a b c [] = exec [] b c []
 jumpIfFalse (n:next) jmp (Boolean True:stk) vars prev = exec next stk vars (n:prev)
 jumpIfFalse (s:l) 0 b c prev = exec l b c (s:prev)
-jumpIfFalse (n:next) jmp [] vars (p:prev) = (Nb 0, [])
+jumpIfFalse (n:next) jmp [] vars (p:prev) = return (Nb 0, [])
 jumpIfFalse (n:next) jmp (s:stk) vars (p:prev) | jmp > length (n:next) = exec [RET] stk vars prev
                                    | jmp < length (p:prev) = exec [RET] stk vars prev
                                    | jmp > 0 = jumpIfFalse next (jmp-1) stk vars (n:p:prev)
@@ -114,3 +128,8 @@ opEq (Boolean a) (Boolean b) = Boolean (a == b)
 opLess :: Value -> Value -> Value
 opLess (Nb a) (Nb b) = Boolean (a < b)
 opLess (Boolean a) (Boolean b) = Boolean (not a && b)
+
+opPrint :: Stack -> IO (Value, Stack)
+opPrint ((Str s):stk) = do
+                        putStrLn s
+                        return (Str s, stk)
